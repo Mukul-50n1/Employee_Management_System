0932d7a9d3a67b0a6d40141a39a063c8d9f00b2b
Mukul Soni <mukul.tout@gmail.com>
2022-05-08 13:55:39 +0530

    edit project

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :memberships
   has_many :employers
   has_many :roles  , through: :memberships
-  has_many :employers , through: :memberships
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
